a231ff30183f191146896e8fa7bf0cb9441ea135
Alexey Chernikov <alexey.chernikov@gmail.com>
2017-09-15 15:31:22 +0500

    Add restore command: restore database from backup

--- lib/techinform.rb
+++ lib/techinform.rb
@@ -1,5 +1,17 @@
-require "techinform/version"
+require 'techinform/version'
+require 'thor'
 
 module Techinform
-  # Your code goes here...
+  class CLI < Thor
+    desc 'restore [type] [filename] [dbname]', 'Restore database from backup'
+    def restore(type, filename, dbname)
+      if type == 'pg'
+        echo "Restoring postgres backup to database #{dbname}..."
+        `tar -xOf #{filename} | pv | gunzip | psql $3 > /dev/null`
+      else
+        echo "Restoring mysql backup to database #{dbname}..."
+        `tar -xOf #{filename} | gunzip | mysql -uroot #{dbname}`
+      end
+    end
+  end
 end
