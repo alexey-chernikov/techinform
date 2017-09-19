f1b16d71dc81ef62c25700a559bd8dfad8802994
Alexey Chernikov <alexey.chernikov@gmail.com>
2017-09-19 15:21:24 +0500

    Fix messages

--- lib/techinform.rb
+++ lib/techinform.rb
@@ -7,10 +7,10 @@ module Techinform
     desc 'restore [type] [filename] [dbname]', 'Restore database from backup'
     def restore(type, filename, dbname)
       if type == 'pg'
-        echo "Restoring postgres backup to database #{dbname}..."
+        puts "Restoring postgres backup to database #{dbname}..."
         `tar -xOf #{filename} | gunzip | pv | psql $3 > /dev/null`
       else
-        echo "Restoring mysql backup to database #{dbname}..."
+        puts "Restoring mysql backup to database #{dbname}..."
         `tar -xOf #{filename} | gunzip | pv | mysql -uroot #{dbname}`
       end
     end
