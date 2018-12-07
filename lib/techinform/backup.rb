2df122fb67d2b622c5d035432e41781d412f165c
Alexey Chernikov <alexey.chernikov@gmail.com>
2018-12-07 17:55:46 +0500

    Encrypt by default if GPGKEY environment variable is set

--- lib/techinform/backup.rb
+++ lib/techinform/backup.rb
@@ -7,13 +7,13 @@ module Techinform
     desc 'mysql [dbname | dbname1,dbname2,... | all] ', 'Backup mysql database/databases, or all databases in separate files'
     def mysql(dbnames)
       (dbnames == 'all' ? mysql_list : dbnames.split(',')).each do |db|
-        MysqlBackup.new(user: ENV['USER'], database: db, password: ENV['PASSWORD']).run
+        MysqlBackup.new(user: ENV['USER'], database: db, password: ENV['PASSWORD'], encrypt: encrypt?).run
       end
     end
 
     desc 'pg_list', 'List of postgres databases'
     def mysql_list
-      dbs = MysqlBackup.new(user: ENV['USER'], password: ENV['PASSWORD']).db_list
+      dbs = MysqlBackup.new(user: ENV['USER'], password: ENV['PASSWORD'], encrypt: encrypt?).db_list
       puts "Available mysql databases: #{dbs.join(', ')}"
       dbs
     end
@@ -21,13 +21,13 @@ module Techinform
     desc 'pg [dbname | dbname1,dbname2,... | all]', 'Backup postgres database/databases or all databases in separate files'
     def pg(dbnames)
       (dbnames == 'all' ? pg_list : dbnames.split(',')).each do |db|
-        PostgreBackup.new(user: ENV['USER'], database: db, password: ENV['PASSWORD']).run
+        PostgreBackup.new(user: ENV['USER'], database: db, password: ENV['PASSWORD'], encrypt: encrypt?).run
       end
     end
 
     desc 'pg_list', 'List of postgres databases'
     def pg_list
-      dbs = PostgreBackup.new(user: ENV['USER'], password: ENV['PASSWORD']).db_list
+      dbs = PostgreBackup.new(user: ENV['USER'], password: ENV['PASSWORD'], encrypt: encrypt?).db_list
       puts "Available postgres databases: #{dbs.join(', ')}"
       dbs
     end
@@ -36,6 +36,12 @@ module Techinform
     def etc
       require_relative '../../lib/backup/etc_backup'
       EtcBackup.new.run
+    end
+
+    private
+
+    def encrypt?
+      ENV['GPGKEY'].present?
     end
   end
 end
