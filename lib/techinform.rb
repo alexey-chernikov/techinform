a45e5374191ffcfa9894ad903a53a58cf1d786e6
Alexey Chernikov <alexey@chernikov.online>
2020-09-24 17:29:17 +0500

    Do not pass excessive spaces to command line

--- lib/techinform.rb
+++ lib/techinform.rb
@@ -33,9 +33,9 @@ module Techinform
           `pv --wait #{filename} | gpg2 --decrypt | #{bzip2} > #{File.basename(filename, '.*') + '.bz2'}`
         elsif encrypted
           puts "Restoring mysql backup to database #{dbname}..."
-          `pv --wait #{filename} | gpg2 --decrypt | mysql #{"-u#{ENV['DBUSER']}" if !ENV['DBUSER'].nil?} #{"-p#{ENV['PASSWORD']}" if !ENV['PASSWORD'].nil?} #{dbname}`
+          `pv --wait #{filename} | gpg2 --decrypt | mysql#{" -u#{ENV['DBUSER']}" if !ENV['DBUSER'].nil?}#{" -p#{ENV['PASSWORD']}" if !ENV['PASSWORD'].nil?} #{dbname}`
         else
-          `pv --wait #{filename} | #{bunzip2} | mysql #{"-u#{ENV['DBUSER']}" if !ENV['DBUSER'].nil?} #{"-p#{ENV['PASSWORD']}" if !ENV['PASSWORD'].nil?} #{dbname}`
+          `pv --wait #{filename} | #{bunzip2} | mysql#{" -u#{ENV['DBUSER']}" if !ENV['DBUSER'].nil?}#{" -p#{ENV['PASSWORD']}" if !ENV['PASSWORD'].nil?} #{dbname}`
         end
       end
     end
